8573e0ca380afb87c7e500fd7329fa34e5270125
MohFox2-lab <MohFox2@hotmail.com>
2026-01-01 22:57:46 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/sabil/',
+  // احذف base أو خله هكذا:
+  base: '/',
   plugins: [react()],
   server: {
     allowedHosts: true // هذا السطر سيسمح للتطبيق بالعمل في أي مكان
